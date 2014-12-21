8804c74fb349171305479590ee53ca26defea2b0
Anirban Chowdhury <anirbans_email@yahoo.com>
2014-12-21 09:49:00 -0800

    updated cachematrix code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These 2 functions work together to cache the inverse of a matrix
+## First function loads a matrix and the second function retrieves it
 
-## Write a short comment describing this function
+## "makeCacheMatrix" takes a square matrix for which an inverse can be calculated
+## There are 4 methods - set_mat, get_mat, set_inv_mat, & get_inv_mat - which
+## allow for a matrix and it's inverse to be loaded/fetched from cache
+## Please check out help on assignment operator ?`<<-` 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv_mat <- NULL
+    set_mat <- function(y) {
+        x <<- y
+        inv_mat <<- NULL
+    }
+    get_mat <- function() { x }
+    set_inv_mat <- function(solve) { inv_mat <<- solve }
+    get_inv_mat <- function() { inv_mat }
+    list(set_mat = set_mat, get_mat = get_mat, set_inv_mat = set_inv_mat, get_inv_mat = get_inv_mat)
 }
 
 
-## Write a short comment describing this function
+## Returns a matrix that is the inverse of matrix loaded by "makeCacheMatrix"
+## Checks if the inverse has already in cache and gets it from cache
+## If inverse is not available then it's calculated
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+          inv_mat <- x$get_inv_mat()
+          if (!is.null(inv_mat)) {
+              message("getting cached inverse matrix")
+              return(inv_mat)
+          }
+          mat_2_op <- x$get_mat()
+          inv_mat <- solve(mat_2_op, ...)
+          x$set_inv_mat(inv_mat)
+          inv_mat        
 }
